986fac2aa73c78f1c42179730dd82031640c6f3f
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-09-29 21:40:38 +0300

    Update Currency.java

--- JavaHomeworkJSON/src/Currency.java
+++ JavaHomeworkJSON/src/Currency.java
@@ -1,15 +1,14 @@
-class Currency {
-    private int id;
-    private String code;
-    private String name_short;
-    private String name_full;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    Currency(int id, String code, String name_short, String name_full) {
-        this.id = id;
-        this.code = code;
-        this.name_short = name_short;
-        this.name_full = name_full;
-    }
+public class Currency {
+    @JsonProperty("id")
+    public int id;
+    @JsonProperty("code")
+    public String code;
+    @JsonProperty("name_short")
+    public String nameShort;
+    @JsonProperty("name_full")
+    public String nameFull;
 
     String getCode() {
         return code;
