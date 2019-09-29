27cf69638e37a2323c1be9e41f5fa99555c202e2
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-09-29 21:37:48 +0300

    Update Company.java

--- JavaHomeworkJSON/src/Company.java
+++ JavaHomeworkJSON/src/Company.java
@@ -1,78 +1,70 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
-class Company {
-    private int id;
-    private String code;
-    private String name_full;
-    private String name_short;
-    private String inn;
-    private CompanyType company_type;
-    private String ogrn;
-    private Date eqrul_date;
-    private Country country;
-    private String fio_head;
-    private String address;
-    private String phone;
-    private String e_mail;
-    private String www;
-    private boolean is_resident;
-    private List<Security> securities;
+public class Company {
+	@JsonProperty("id")
+	private int id;
+	@JsonProperty("code")
+	private String code;
+	@JsonProperty("name_full")
+	private String nameFull;
+	@JsonProperty("name_short")
+	private String nameShort;
+	@JsonProperty("inn")
+	private String inn;
+	@JsonProperty("company_type")
+	private CompanyType companyType;
+	@JsonProperty("ogrn")
+	private String ogrn;
+	@JsonProperty("egrul_date")
+	private Date eqrulDate;
+	@JsonProperty("country")
+	private Country country;
+	@JsonProperty("fio_head")
+	private String fioHead;
+	@JsonProperty("address")
+	private String address;
+	@JsonProperty("phone")
+	private String phone;
+	@JsonProperty("e_mail")
+	private String eMail;
+	@JsonProperty("www")
+	private String www;
+	@JsonProperty("is_resident")
+	private boolean isResident;
+	@JsonProperty("securities")
+	private List<Security> securities;
 
-    Company(int id, String code, String name_full, String name_short, String inn, CompanyType company_type,
-            String ogrn, Date eqrul_date, Country country, String fio_head, String address,
-            String phone, String e_mail, String www, boolean is_resident) {
-        this.id = id;
-        this.code = code;
-        this.name_full = name_full;
-        this.name_short = name_short;
-        this.inn = inn;
-        this.company_type = company_type;
-        this.ogrn = ogrn;
-        this.eqrul_date = eqrul_date;
-        this.country = country;
-        this.fio_head = fio_head;
-        this.address = address;
-        this.phone = phone;
-        this.e_mail = e_mail;
-        this.www = www;
-        this.is_resident = is_resident;
-    }
+	public void setSecurities(List<Security> securities) {
+		this.securities = securities;
+	}
 
-    void setSecurities(List<Security> securities) {
-        this.securities = securities;
-    }
+	public void printInfo() {
+		System.out.println("Краткое название: " + nameShort + "\nДата основания: " + new SimpleDateFormat("dd/mm/yyyy")
+				.format(eqrulDate));
+	}
 
-    void printInfo() {
-        System.out.println("Краткое название: " + name_short + "\nДата основания: "
-                + new SimpleDateFormat("dd/mm/yyyy").format(eqrul_date));
-    }
+	public void printFinishedSecurities() {
+		securities.forEach(security -> {
+			if (security.isExpired()) {
+				security.printInfo();
+			}
+		});
+	}
 
-    void printFinishedSecurities() {
-        securities.forEach(security -> {
-            if (security.isExpired()) {
-                security.printInfo();
-            }
-        });
-    }
+	public int getFinishedSecuritiesCount() {
+		return (int) securities.stream().filter(Security::isExpired).count();
+	}
 
-    int getFinishedSecuritiesCount() {
-        return (int)securities.stream()
-                .filter(Security::isExpired)
-                .count();
-    }
+	public boolean hasCreatedAfterDate(Date date) {
+		return eqrulDate.after(date);
+	}
 
-    boolean hasCreatedAfterDate(Date date) {
-        return eqrul_date.after(date);
-    }
-
-    void printInfoByCode(String code) {
-        securities.stream()
-                .filter(security -> security.getCurrencyCode().equals(code))
-                .forEach(Security::printSecurityInfo);
-    }
+	public void printInfoByCode(String code) {
+		securities.stream().filter(security -> security.getCurrencyCode().equals(code))
+				  .forEach(Security::printSecurityInfo);
+	}
 }
